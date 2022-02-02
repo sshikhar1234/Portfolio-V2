149e0719baa0b3f619d9d0e9be77c6330647b1cb
sshikhar1234 <sshikharshah@gmail.com>
2022-02-02 14:08:35 -0500

    Changed page title and favicon

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import Portfolio from './Portfolio';
 import './styles/base.scss';
 
+class Doc extends React.Component{
+  componentDidMount(){
+    document.title = "Shikhar Shah • DevOps • Cloud • Automation"
+  }
+  render(){
+    return(
+      <b> </b>
+    )
+  }
+}
+
 ReactDOM.render(
   <>
+    <Doc />
     <Portfolio />
   </>,
   document.querySelector('#root')

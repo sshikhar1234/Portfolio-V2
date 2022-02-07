0657bb20f5c40c768294cc46dd1f18839d6d5e12
Shikhar Shah <sshikharshah@gmail.com>
2022-02-07 14:41:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/base.scss';
 class Doc extends React.Component{
   componentDidMount(){
     document.title = "Shikhar Shah • DevOps • Cloud • Automation"
+    document.head.append = <><script async defer data-domain="shikharshah.ca" src="https://plausible.io/js/plausible.js"></script></>
   }
   render(){
     return(
